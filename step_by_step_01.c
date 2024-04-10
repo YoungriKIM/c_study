eb1a9e1fb206a5fec2e06ceb0280d8ed12bf2ea0
YoungriKIM <76423415+YoungriKIM@users.noreply.github.com>
2024-04-10 14:36:25 +0900

    Update step_by_step_01.c

--- step_by_step_01.c
+++ step_by_step_01.c
@@ -1,98 +1,99 @@
-// #include <stdio.h>
-
-// C Program to Print an Integer entered by user
-// int main ()
-// {
-//   int num;
-//   printf ("Enter an integer number: ");
-//   scanf ("%d", &num);
-//   printf ("Entered number is %d", num);
-//   return 0;
-// }
+/*
+#include <stdio.h>
+C Program to Print an Integer entered by user
+int main ()
+{
+  int num;
+  printf ("Enter an integer number: ");
+  scanf ("%d", &num);
+  printf ("Entered number is %d", num);
+  return 0;
+}
 // ---------------------------------------------------------------------
 
-// C Program to add two numbers
-// int main()
-// {
-//     int num1, num2, sum;
-//     printf("첫 번째 숫자 입력: ");
-//     scanf("%d", &num1);
-//     printf("두 번째 숫자 입력: ");
-//     scanf("%d", &num2);
-//     sum = num1 + num2;
-//     printf("두 수의 합은 %d", sum);
-//     return 0;
-// }
+C Program to add two numbers
+int main()
+{
+    int num1, num2, sum;
+    printf("첫 번째 숫자 입력: ");
+    scanf("%d", &num1);
+    printf("두 번째 숫자 입력: ");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("두 수의 합은 %d", sum);
+    return 0;
+}
 // ---------------------------------------------------------------------
 
-// C Program to multiply two numbers
-// int main()
-// {
-//     int num1, num2, mul;
-//     scanf("%d", &num1);
-//     scanf("%d", &num2);
-//     mul = num1 * num2;
-//     printf("두 수의 곱은 %d", mul);
-//     return 0;
-// }
+C Program to multiply two numbers
+int main()
+{
+    int num1, num2, mul;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    mul = num1 * num2;
+    printf("두 수의 곱은 %d", mul);
+    return 0;
+}
 // ---------------------------------------------------------------------
 
-// C Program to calculate average of numbers
-// int main()
-// {
-//     int num, sum, i;
-//     float average;
+C Program to calculate average of numbers
+int main()
+{
+    int num, sum, i;
+    float average;
 
-//     printf("2개 숫자 입력: ");
+    printf("2개 숫자 입력: ");
     
-//     for(i=0; i<2; i++){
-//         scanf("%d", &num);
-//         sum += num;
-//     }
+    for(i=0; i<2; i++){
+        scanf("%d", &num);
+        sum += num;
+    }
 
-//     average = sum/(float)2;
-//     printf("평균은 %.2f", average);
+    average = sum/(float)2;
+    printf("평균은 %.2f", average);
     
-//     return 0;
-// }
+    return 0;
+}
 // ---------------------------------------------------------------------
 
-// C Program to print ASCII value of character
-// #include <stdio.h>
+C Program to print ASCII value of character
+#include <stdio.h>
 
-// int main()
-// {
-//     char ch;
-//     printf("문자 입력: ");
-//     scanf("%c", &ch);
+int main()
+{
+    char ch;
+    printf("문자 입력: ");
+    scanf("%c", &ch);
     
-//     printf("ASCII value of '%c' is '%d'", ch, ch);
+    printf("ASCII value of '%c' is '%d'", ch, ch);
     
-//     return 0;
-// }
+    return 0;
+}
 // ---------------------------------------------------------------------
+*/
 
-// C Program to print quotient and remainder
-// #include <stdio.h>
+C Program to print quotient and remainder
+#include <stdio.h>
 
-// int main()
-// {
-//     int num1, num2, q, rem;
-//     printf("첫 번째 숫자: ");
-//     scanf("%d", &num1);
-//     printf("두 번째 숫자: ");
-//     scanf("%d", &num2);
+int main()
+{
+    int num1, num2, q, rem;
+    printf("첫 번째 숫자: ");
+    scanf("%d", &num1);
+    printf("두 번째 숫자: ");
+    scanf("%d", &num2);
     
-//     if(num2==0){
-//         printf("0으로 나눌 수 없어요.");
-//         return 1;
-//     }
+    if(num2==0){
+        printf("0으로 나눌 수 없어요.");
+        return 1;
+    }
     
-//     q = num1/num2, rem = num1%num2;
-//     printf("몫은 %d, 나머지는 %d", q, rem);
+    q = num1/num2, rem = num1%num2;
+    printf("몫은 %d, 나머지는 %d", q, rem);
     
-//     return 0;
-// }
+    return 0;
+}
 // ---------------------------------------------------------------------
 
 
